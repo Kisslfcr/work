4f50a35ed86faa55bc697ef32ff1f4e05930d1e9
Kisslfcr <840552259@qq.com>
2020-06-28 17:05:22 +0800

    phase 7 challege 22

---
+++ phase7/challege22.py
@@ -0,0 +1,20 @@
+# shiyanlou:Code/ $ cat analysis.py                                                                                        [17:01:12]
+import json
+import pandas as pd
+
+def analysis(file, user_id):
+    times = 0
+    minutes = 0
+    with open(file) as f:
+        fj = json.loads(f.read())
+    df = pd.DataFrame(fj)    
+    df1 = df[df['user_id'] == user_id]
+    print(df1)
+    times = df1.shape[1]
+    minutes = df1.minutes.sum()
+
+    return times, minutes
+
+if __name__ == '__main__':
+    an1 = analysis('user_study.json',5348)
+    print(an1)
